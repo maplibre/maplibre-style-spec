f45adf217f935702ab7558b22d14119d21ec8263
Birk Skyum <birk.skyum@pm.me>
2023-03-06 21:38:33 +0100

    String template highligting

---
+++ docs/src/content/docs/highligting/root1.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {highlightJSON} from '../../../components/style-spec/prism_highlight';
+
+function RootPrism() {
+
+    const version = 10;
+
+    const mdString = `{
+    "version": {version},
+    "name": "Mapbox Streets",
+    "sprite": "mapbox://sprites/mapbox/streets-v${version}",
+    "glyphs": "mapbox://fonts/mapbox/{fontstack}/{range}.pbf",
+    "sources": {...},
+    "layers": [...]
+  }`;
+
+
+    const highligted = highlightJSON(mdString);
+
+    return (
+        <>
+            <p>{highligted}</p>
+        </>
+    );
+}
+
+export default RootPrism;
