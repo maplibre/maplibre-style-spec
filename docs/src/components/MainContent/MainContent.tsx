4347d96a8521bbed0395324482fbc4b44f9b06da
Birk Skyum <birk.skyum@pm.me>
2023-04-03 16:19:47 +0200

    fix edit on github button

--- docs/src/components/MainContent/MainContent.tsx
+++ docs/src/components/MainContent/MainContent.tsx
@@ -1,3 +1,4 @@
+import {useLocation} from 'solid-start';
 import {TableOfContents} from '../TableOfContents/TableOfContents';
 import {Accordion, AccordionHeader} from '../accordion/accordion';
 import style from './maincontent.module.scss';
@@ -8,6 +9,9 @@ interface MainContentProps {
 }
 
 export function MainContent(props: MainContentProps) {
+
+    const location = useLocation();
+
     return (
         <main class={`${style.mainContentContainer} ${props.class}`}>
 
@@ -25,6 +29,8 @@ export function MainContent(props: MainContentProps) {
                 <div class={style.scrollToTop} onClick={() => {
                     document.documentElement.scrollTop = 0;
                 }}><i class="fa-solid fa-arrow-up"></i></div>
+
+                <a class={style.githubLink} target="_blank" href={`https://github.com/maplibre/maplibre-gl-style-spec/blob/main/docs/src/routes${location.pathname === '/' ? '/index' : location.pathname}.tsx`}><i class="fa-brands fa-github"></i> Edit page on GitHub</a>
             </div>
         </main>
     );
