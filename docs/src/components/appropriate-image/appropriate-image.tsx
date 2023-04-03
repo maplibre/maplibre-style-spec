4a86efe2bb48632777d50b71423ebdb35f56fbc3
Birk Skyum <birk.skyum@pm.me>
2023-04-03 22:48:43 +0200

    remove comment

--- docs/src/components/appropriate-image/appropriate-image.tsx
+++ docs/src/components/appropriate-image/appropriate-image.tsx
@@ -1,14 +1,3 @@
-// import { scopeAppropriateImage } from '@mapbox/appropriate-images-react';
-// import imageConfig from '../img/dist/image.config.json'; // eslint-disable-line
-// // image.config.json is generated on build
-
-// // See https://github.com/mapbox/appropriate-images-react#appropriateimage
-// // The required prop is `imageId`, which must correspond to a key in the
-// // imageConfig.
-// const AppropriateImage = scopeAppropriateImage(imageConfig, {
-//     transformUrl: (url) => require(`img/s/${url}`).default // eslint-disable-line
-// });
-
 import style from './appropriate-image.module.scss';
 
 export function AppropriateImage ({imageId}:any) {
