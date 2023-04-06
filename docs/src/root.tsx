07f890ed1794b9362318507f537735623c211ea5
Birk Skyum <birk.skyum@pm.me>
2023-04-06 15:12:08 +0200

    adjust headers

--- docs/src/root.tsx
+++ docs/src/root.tsx
@@ -31,10 +31,7 @@ export default function Root() {
                 <Title>MapLibre Styles Docs</Title>
                 <Meta charset="utf-8" />
                 <Meta name="viewport" content="width=device-width, initial-scale=1" />
-                <Link rel="preconnect" href="https://fonts.googleapis.com" />
-                <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <Link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet" />
-
                 <Link href="https://unpkg.com/@fortawesome/fontawesome-free@6.4.0/css/fontawesome.css" rel="stylesheet" />
                 <Link href="https://unpkg.com/@fortawesome/fontawesome-free@6.4.0/css/brands.css" rel="stylesheet" />
                 <Link href="https://unpkg.com/@fortawesome/fontawesome-free@6.4.0/css/solid.css" rel="stylesheet" />
