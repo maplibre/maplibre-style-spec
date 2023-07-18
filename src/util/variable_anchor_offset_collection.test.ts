1ee34c20d95632b76a00aaef40eac67c57b9d84a
Derek Westcott <drwestco@gmail.com>
2023-07-18 14:24:36 -0700

    Code coverage

--- src/util/variable_anchor_offset_collection.test.ts
+++ src/util/variable_anchor_offset_collection.test.ts
@@ -13,9 +13,13 @@ describe('VariableAnchorOffsetCollection', () => {
         expect(VariableAnchorOffsetCollection.parse(['top'])).toBeUndefined();
         expect(VariableAnchorOffsetCollection.parse(['top', 'bottom'])).toBeUndefined();
         expect(VariableAnchorOffsetCollection.parse(['top', 3] as any)).toBeUndefined();
+        expect(VariableAnchorOffsetCollection.parse(['Dennis', [2, 2]])).toBeUndefined();
         expect(VariableAnchorOffsetCollection.parse(['top', [2, 2]])).toEqual(new VariableAnchorOffsetCollection(['top', [2, 2]]));
         expect(VariableAnchorOffsetCollection.parse(['top', [2, 2], 'bottom'])).toBeUndefined();
         expect(VariableAnchorOffsetCollection.parse(['top', [2, 2], 'bottom', [3, 3]])).toEqual(new VariableAnchorOffsetCollection(['top', [2, 2], 'bottom', [3, 3]]));
+
+        const identity = new VariableAnchorOffsetCollection(['top', [2, 2]]);
+        expect(VariableAnchorOffsetCollection.parse(identity)).toBe(identity);
     });
 
     test('VariableAnchorOffsetCollection#toString', () => {
