0cab64246cb04b62ef5edc97c0c48a9c975abf0a
HarelM <harel.mazor@gmail.com>
2024-02-02 12:06:59 +0200

    Add missing sky in diff results

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -467,4 +467,15 @@ test('diff', () => {
     } as StyleSpecification)).toEqual([
         {command: 'setTerrain', args: [{source: 'maplibre-dem', exaggeration: 1.5}]},
     ]);
+
+    expect(diffStyles({
+    } as StyleSpecification,
+    {
+        sky: {
+            "fog-color": 'green',
+            "fog-blend": 0.2
+        }
+    } as StyleSpecification)).toEqual([
+        {command: 'setSky', args: [{"fog-color": 'green', "fog-blend": 0.2}]},
+    ]);
 });
