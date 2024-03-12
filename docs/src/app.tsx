94b8d52efab87ca8c31839082d564349b10b8b06
Birk Skyum <birk.skyum@pm.me>
2024-03-12 01:21:42 +0100

    small fixes

--- docs/src/app.tsx
+++ docs/src/app.tsx
@@ -13,7 +13,7 @@ export default function Root() {
                 <MetaProvider>
                     <Title>MapLibre Style Spec</Title>
                     <Meta charset="utf-8" />
-                    <Meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <Meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
                     <Link
                         href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
                         rel="stylesheet"
