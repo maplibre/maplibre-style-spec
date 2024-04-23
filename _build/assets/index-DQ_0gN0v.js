7646730f371d9c2a26fe45c6d7dd2a81e2869d4a
HarelM <harel.mazor@gmail.com>
2024-04-23 20:42:24 +0000

    Deploying to gh-pages from @ maplibre/maplibre-style-spec@87b9f0eb55ea75cae5b23cddf47179bceafdf765 🚀

---
+++ _build/assets/index-DQ_0gN0v.js
@@ -0,0 +1,16 @@
+import{g as c,k as r,i as o,c as s,t as f}from"./web-CPLIbZ3z.js";import{M as g}from"./markdown-CADDdR9C.js";import{s as d}from"./v8-DAYV0zQS.js";import{I as h}from"./items-S3fZRJRj.js";import"./property-CGgLBG3n.js";import"./components-B_iav0dM.js";import"./subtitle-kxhuKEHP.js";var $=f("<div><!$><!/><!$><!/>");function k(){const i=`# Root
+Root level properties of a MapLibre style specify the map's layers, tile sources and other resources, and default values for the initial camera position when not specified elsewhere.
+
+
+\`\`\`json
+{
+    "version": 8,
+    "name": "MapLibre Demo Tiles",
+    "sprite": "https://demotiles.maplibre.org/styles/osm-bright-gl-style/sprite",
+    "glyphs": "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
+    "sources": {... },
+    "layers": [...]
+}
+\`\`\`
+
+`;return(()=>{var e=c($),a=e.firstChild,[t,l]=r(a.nextSibling),n=t.nextSibling,[p,m]=r(n.nextSibling);return o(e,s(g,{content:i}),t,l),o(e,s(h,{headingLevel:"2",get entry(){return d.$root}}),p,m),e})()}export{k as default};
