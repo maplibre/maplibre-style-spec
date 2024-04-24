bda44a91837a700bf4e87bad33fcb640f3d8ae07
birkskyum <74932975+birkskyum@users.noreply.github.com>
2024-04-24 12:29:27 +0000

    Deploying to gh-pages from @ maplibre/maplibre-style-spec@85a63c49c471c534cbd1af3c02e075feb1e94f69 🚀

--- _build/assets/index-BPDD-uSU.js
+++
@@ -1,9 +0,0 @@
-import{g,k as r,i as n,c as i,t as c}from"./web-CPLIbZ3z.js";import{M as d}from"./markdown-CADDdR9C.js";import{s as o}from"./v8-DAYV0zQS.js";import{I as f}from"./items-S3fZRJRj.js";import"./property-CGgLBG3n.js";import"./components-B_iav0dM.js";import"./subtitle-kxhuKEHP.js";var _=c("<div><!$><!/><!$><!/>");function M(){const a=`# Terrain
-
-Add 3D terrain to the map.
-
-\`\`\`json
-"terrain": ${JSON.stringify(o.$root.terrain.example,null,2)}
-\`\`\`
-
-`;return(()=>{var t=g(_),m=t.firstChild,[e,l]=r(m.nextSibling),s=e.nextSibling,[p,$]=r(s.nextSibling);return n(t,i(d,{content:a}),e,l),n(t,i(f,{headingLevel:"3",get entry(){return o.terrain}}),p,$),t})()}export{M as default};
---
+++ _build/assets/index-C69DS08Z.js
@@ -0,0 +1,16 @@
+import{g as c,k as r,i as o,c as s,t as f}from"./web-DEAY107g.js";import{M as g}from"./markdown-CO39_19j.js";import{s as d}from"./v8-DAYV0zQS.js";import{I as h}from"./items-DJu72sNC.js";import"./property-F8Zh0-De.js";import"./components-CS85MpCs.js";import"./subtitle-B5vsGLij.js";var $=f("<div><!$><!/><!$><!/>");function k(){const i=`# Root
+Root level properties of a MapLibre style specify the map's layers, tile sources and other resources, and default values for the initial camera position when not specified elsewhere.
+
+
+\`\`\`json
+{
+    "version": 8,
+    "name": "MapLibre Demo Tiles",
+    "sprite": "https://demotiles.maplibre.org/styles/osm-bright-gl-style/sprite",
+    "glyphs": "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
+    "sources": {... },
+    "layers": [...]
+}
+\`\`\`
+
+`;return(()=>{var e=c($),a=e.firstChild,[t,l]=r(a.nextSibling),n=t.nextSibling,[p,m]=r(n.nextSibling);return o(e,s(g,{content:i}),t,l),o(e,s(h,{headingLevel:"2",get entry(){return d.$root}}),p,m),e})()}export{k as default};
