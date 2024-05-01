beb2948ae732ea321582ba5db5ccbc377aa11cb2
HarelM <harel.mazor@gmail.com>
2024-05-01 13:02:20 +0300

    Fix lint

--- build/generate-docs.ts
+++ build/generate-docs.ts
@@ -449,7 +449,7 @@ function createExpressionsContent() {
             content += `\n### ${key}\n`;
             content += `${value.doc}\n`;
             value.example.syntax.method.unshift(`"${key}"`);
-            content += `\nSyntax:\n\`\`\`js\n[${value.example.syntax.method.join(", ")}]: ${value.example.syntax.result}\n\`\`\`\n`;
+            content += `\nSyntax:\n\`\`\`js\n[${value.example.syntax.method.join(', ')}]: ${value.example.syntax.result}\n\`\`\`\n`;
             content += `\nExample:\n\`\`\`json\n"some-property": ${JSON.stringify(value.example.value)}\n\`\`\`\n`;
             content += sdkSupportToMarkdown(value['sdk-support'] as any);
             content += '\n';
