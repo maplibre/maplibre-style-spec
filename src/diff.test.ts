00a4185678133617b40d4455a08731b7afa608a5
Harel M <harel.mazor@gmail.com>
2024-05-30 15:56:59 +0300

    Update diff.test.ts

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -557,7 +557,7 @@ describe('diff', () => {
         {
             sky: {
                 'fog-color': 'green',
-                'fog-blend': 0.2
+                'fog-ground-blend': 0.2
             }
         } as StyleSpecification)).toEqual([
             {command: 'setSky', args: [{'fog-color': 'green', 'fog-blend': 0.2}]},
