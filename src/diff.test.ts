4085cd4146fc4dab1b6ca82be17219e0bcbacf45
Birk Skyum <birk.skyum@pm.me>
2024-11-02 13:13:00 +0100

    Rename adaptive globe from `globe` to `globe-mercator`

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -607,10 +607,10 @@ describe('diff', () => {
         } as StyleSpecification,
         {
             projection: {
-                type: 'globe'
+                type: 'globe-mercator'
             }
         } as StyleSpecification)).toEqual([
-            {command: 'setProjection', args: [{type: 'globe'}]},
+            {command: 'setProjection', args: [{type: 'globe-mercator'}]},
         ]);
     });
 });
