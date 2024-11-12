277b82f23d5bfe40de6e9c0e12c86c8b2646f704
Birk Skyum <birk.skyum@pm.me>
2024-11-12 22:42:55 +0100

    fix test name

--- src/util/projection.test.ts
+++ src/util/projection.test.ts
@@ -2,7 +2,7 @@ import {Projection} from './projection';
 
 describe('Projection class', () => {
 
-    test('should serialize to rgba format', () => {
+    test('should serialize projection, with [from, to, transition]', () => {
         expect(`${new Projection('mercator', 'vertical-perspective', 1)}`).toBe('["mercator", "vertical-perspective", 1]');
         expect(`${new Projection('vertical-perspective', 'mercator', 0.3)}`).toBe('["vertical-perspective", "mercator", 0.3]');
     });
