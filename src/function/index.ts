6ac272551edfd82947731239bf4e213e367d4816
HarelM <harel.mazor@gmail.com>
2024-11-13 14:26:19 +0200

    Remove unwanted changes

--- src/function/index.ts
+++ src/function/index.ts
@@ -23,6 +23,7 @@ export function createFunction(parameters, propertySpec) {
     const featureDependent = zoomAndFeatureDependent || parameters.property !== undefined;
     const zoomDependent = zoomAndFeatureDependent || !featureDependent;
     const type = parameters.type || (supportsInterpolation(propertySpec) ? 'exponential' : 'interval');
+
     if (isColor || propertySpec.type === 'padding') {
         const parseFn = isColor ? Color.parse : Padding.parse;
 
