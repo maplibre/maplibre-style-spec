1b2dec0b7db8bb3bf1192312267d6884ac748b1a
Birk Skyum <birk.skyum@pm.me>
2024-11-16 20:29:13 +0100

    remove * as

--- build/release-notes.js
+++ build/release-notes.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs';
+import {readFileSync} from 'fs';
 import semver from 'semver';
 
 const changelogPath = 'CHANGELOG.md';
-const changelog = fs.readFileSync(changelogPath, 'utf8');
+const changelog = readFileSync(changelogPath, 'utf8');
 
 /*
   Parse the raw changelog text and split it into individual releases.
