19b65b609019c5687bceda2d33347f9cb2a71715
Birk Skyum <birk.skyum@pm.me>
2024-11-16 21:02:16 +0100

    named import

--- src/validate/validate_projectiondefinition.test.ts
+++ src/validate/validate_projectiondefinition.test.ts
@@ -1,58 +1,58 @@
-import validateProjection from './validate_projectiondefinition';
+import {validateProjectionDefinition} from './validate_projectiondefinition';
 
 describe('validateProjection function', () => {
 
     const key = 'sample_projection_key';
 
     test('should return error when projection is not a string or array', () => {
-        expect(validateProjection({key, value: ''})).toHaveLength(0);
+        expect(validateProjectionDefinition({key, value: ''})).toHaveLength(0);
 
-        expect(validateProjection({key, value: 0})).toMatchObject([
+        expect(validateProjectionDefinition({key, value: 0})).toMatchObject([
             {message: `${key}: projection expected, invalid type "number" found`},
         ]);
-        expect(validateProjection({key, value: {}})).toMatchObject([
+        expect(validateProjectionDefinition({key, value: {}})).toMatchObject([
             {message: `${key}: projection expected, invalid type "object" found`},
         ]);
-        expect(validateProjection({key, value: false})).toMatchObject([
+        expect(validateProjectionDefinition({key, value: false})).toMatchObject([
             {message: `${key}: projection expected, invalid type "boolean" found`},
         ]);
-        expect(validateProjection({key, value: null})).toMatchObject([
+        expect(validateProjectionDefinition({key, value: null})).toMatchObject([
             {message: `${key}: projection expected, invalid type "null" found`},
         ]);
-        expect(validateProjection({key, value: undefined})).toMatchObject([
+        expect(validateProjectionDefinition({key, value: undefined})).toMatchObject([
             {message: `${key}: projection expected, invalid type "undefined" found`},
         ]);
     });
     
     test('Should error when projection is an invalid projection transition', () => {
-        const errors = validateProjection({value: [3, 'mercator', 0.3], key});
+        const errors = validateProjectionDefinition({value: [3, 'mercator', 0.3], key});
         expect(errors).toMatchObject([
             {message: `${key}: projection expected, invalid array [3,\"mercator\",0.3] found`},
         ]);
     });
 
     test('Should allow string', () => {
-        const errors = validateProjection({value: 'mercator', key});
+        const errors = validateProjectionDefinition({value: 'mercator', key});
         expect(errors).toHaveLength(0);
     });
 
     test('Should return no errors when projection is valid projection transition', () => {
-        const errors = validateProjection({value: ['mercator', 'mercator', 0.3], key});
+        const errors = validateProjectionDefinition({value: ['mercator', 'mercator', 0.3], key});
         expect(errors).toHaveLength(0);
     });
 
     test('Should return no errors when projection is valid interpolation-projection expression', () => {
-        const errors = validateProjection({value: ['interpolate', ['linear'], ['zoom'], 0, 'mercator', 5, 'vertical-perspective'], key});
+        const errors = validateProjectionDefinition({value: ['interpolate', ['linear'], ['zoom'], 0, 'mercator', 5, 'vertical-perspective'], key});
         expect(errors).toHaveLength(0);
     });
 
     test('Should return no errors when projection is valid step expression', () => {
-        const errors = validateProjection({value: ['step', ['zoom'], 'vertical-perspective', 10, 'mercator'], key});
+        const errors = validateProjectionDefinition({value: ['step', ['zoom'], 'vertical-perspective', 10, 'mercator'], key});
         expect(errors).toHaveLength(0);
     });
 
     test('Should return no errors when projection is valid step expression with a transition', () => {
-        const errors = validateProjection({value: ['step', ['zoom'], ['vertical-perspective', 'mercator', 0.5], 10, 'mercator'], key});
+        const errors = validateProjectionDefinition({value: ['step', ['zoom'], ['vertical-perspective', 'mercator', 0.5], 10, 'mercator'], key});
         expect(errors).toHaveLength(0);
     });
 
