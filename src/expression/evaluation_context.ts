89d3c87b82e0ed777171229aa649bad2913c538b
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2025-03-08 04:45:26 +0200

    Use Map instead of an object in EvaluationContext's cache

--- src/expression/evaluation_context.ts
+++ src/expression/evaluation_context.ts
@@ -13,16 +13,14 @@ export class EvaluationContext {
     availableImages: Array<string>;
     canonical: ICanonicalTileID;
 
-    _parseColorCache: {[_: string]: Color};
+    _parseColorCache: Map<string, Color>;
 
     constructor() {
         this.globals = null;
         this.feature = null;
         this.featureState = null;
         this.formattedSection = null;
-        // the cache keys are user controlled (from the source JSON), so
-        // avoid prototype pollution by creating a record with a null prototype
-        this._parseColorCache = Object.create(null) as {[_: string]: Color};
+        this._parseColorCache = new Map<string, Color>();
         this.availableImages = null;
         this.canonical = null;
     }
@@ -48,9 +46,10 @@ export class EvaluationContext {
     }
 
     parseColor(input: string): Color {
-        let cached = this._parseColorCache[input];
+        let cached = this._parseColorCache.get(input);
         if (!cached) {
-            cached = this._parseColorCache[input] = Color.parse(input);
+            cached = Color.parse(input);
+            this._parseColorCache.set(input, cached);
         }
         return cached;
     }
