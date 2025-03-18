dba4c4ffcd314534e6fef06889f9a26414c6fe85
Zbigniew Matysek <zbigniew.matysek@tomtom.com>
2025-03-18 13:15:31 +0100

    improve docs

--- build/generate-docs.ts
+++ build/generate-docs.ts
@@ -20,7 +20,7 @@ type JsonSdkSupport = {
 type JsonObject = {
     required?: boolean;
     units?: string;
-    default?: string | number | boolean;
+    default?: string | number | boolean | {};
     type: string;
     doc: string;
     requires?: any[];
@@ -227,7 +227,7 @@ function convertPropertyToMarkdown(key: string, value: JsonObject, keyPrefix = '
         markdown += `Units in ${value.units}. `;
     }
     if (value.default !== undefined) {
-        markdown += `Defaults to \`${value.default}\`. `;
+        markdown += `Defaults to \`${JSON.stringify(value.default)}\`. `;
     }
     if (value.requires) {
         markdown += requiresToMarkdown(value.requires);
