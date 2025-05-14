fb9b8e6c80eb73aab184bcffa0fc1b01e8cacb73
Birk Skyum <birk.skyum@pm.me>
2025-05-14 17:13:59 +0200

    remove jsdom

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
     test: {
         name: 'unit',
-        environment: 'jsdom',
+        environment: 'node',
         environmentOptions: {
             jsdom: {
                 url: 'http://localhost/',
