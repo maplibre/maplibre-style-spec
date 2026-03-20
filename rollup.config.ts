9c5b7f47355da8a6630269922a821dfdb8a3fb5a
Birk Skyum <birk.skyum@pm.me>
2026-03-20 10:31:19 +0100

    cleanup deps

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,7 +4,7 @@ import shebang from 'rollup-plugin-preserve-shebang';
 import {defineConfig} from 'rolldown';
 
 const rollupPlugins = [
-    minifyStyleSpec(),
+		minifyStyleSpec(),
     // https://github.com/zaach/jison/issues/351
     replace({
         preventAssignment: true,
@@ -18,6 +18,10 @@ const rollupPlugins = [
 
 const config = defineConfig([
     {
+				resolve: {
+						mainFields: ['browser', 'module', 'main']
+				},
+				external: ['fs'],
         input: './src/index.ts',
         output: [
             {
@@ -38,6 +42,10 @@ const config = defineConfig([
         plugins: rollupPlugins
     },
     {
+				resolve: {
+						mainFields: ['browser', 'module', 'main']
+				},
+				external: ['fs'],
         input: './bin/gl-style-format.ts',
         output: [
             {
@@ -58,6 +66,10 @@ const config = defineConfig([
         plugins: [...rollupPlugins, shebang()]
     },
     {
+				resolve: {
+						mainFields: ['browser', 'module', 'main']
+				},
+				external: ['fs'],
         input: './bin/gl-style-migrate.ts',
         output: [
             {
@@ -78,6 +90,11 @@ const config = defineConfig([
         plugins: [...rollupPlugins, shebang()]
     },
     {
+				resolve: {
+						mainFields: ['browser', 'module', 'main']
+				},
+				external: ['fs'],
+
         input: './bin/gl-style-validate.ts',
         output: [
             {
